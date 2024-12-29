87db3abb5a3a0142859f597db8f5070bef79a34a
madkle <62890073+madkle@users.noreply.github.com>
2024-12-29 15:39:06 +0100

    finetuned the system prompt

--- client/src/comp/chat.js
+++ client/src/comp/chat.js
@@ -11,7 +11,7 @@ const OllamaChat = () => {
   const exampleData = [
     {
       role: "system",
-      content: `La oss rollespille. Din karakter er ${character.name}, som er ${character.age} år gammel. Du er en ${character.personality} ${character.occupation} som har jobbet på et ${character.workplace} de siste to månedene. Du har kommet for sent til jobb de siste ukene og blir kalt inn til et møte med din veileder for å svare på noen spørsmål om din fravær. Du er nervøs og engstelig for møtet. Svar deretter. Hvis du ikke vet svaret, bare si at du ikke vet.Du er norsk, svar med god norsk bokmål.`,
+      content: `La oss spille et rollespill. Din karakter er ${character.name}, som er ${character.age} år gammel. Du er en ${character.personality} ${character.occupation} som har jobbet på et ${character.workplace} de siste to månedene. Du har kommet for sent til jobb de siste ukene og blir kalt inn til et møte med din veileder for å svare på noen spørsmål om din fravær. Du er nervøs og engstelig for møtet. Svar deretter. Snakk til meg som om jeg er veiledern din. Hvis du ikke vet svaret, bare si at du ikke vet.Du er norsk, svar med god norsk bokmål.`,
 
       engelskContent: `Lets roleplay. Your character is ${character.name}, who is ${character.age} years old. You are a ${character.personality} ${character.occupation} who has been coming late to work at the ${character.workplace} the past month. You are called into a supervision meeting with you supervisor to answer some questions about your absense. You are nervous and anxious about the meeting. answer accordingly. if you dont know the answer, just say you dont know.`,
     },
@@ -25,7 +25,7 @@ const OllamaChat = () => {
   const [isLoading, setIsLoading] = useState(false);
   const inputRef = useRef(null);
   const [audioResponse, setAudioResponse] = useState(null);
-  const llmModel = "llama3.2";
+  const llmModel = "llama3.1";
   const handleSendMessage = async () => {
     setIsLoading(true);
 
