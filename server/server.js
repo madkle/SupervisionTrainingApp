0083c7c6605a07c7cf6732d231c29599ced996e2
madkle <Mads.madsen.klepper@gmail.com>
2025-03-05 18:41:42 +0100

    api test

--- server/server.js
+++ server/server.js
@@ -16,7 +16,9 @@ app.use(express.json());
 
 app.use('/ollama', ollamaRoutes)
 app.use('/openai', openAIRoutes)
-
+app.get("/test", (req, res) => {
+  res.json({ message: "API is working!" });
+});
 
 
 // Start the server
