88e83f863dca8d2a41295a621d18d2cd7c5b7109
madkle <Mads.madsen.klepper@gmail.com>
2025-04-15 14:36:53 +0200

    cleanup

--- client/src/comp/reportPage/feedback.jsx
+++ client/src/comp/reportPage/feedback.jsx
@@ -48,24 +48,6 @@ const FeedbackDisplay = ({ response, scenario }) => {
           })}
         </ul>
       </section>
-
-      {/* <h1>{data.title}</h1> 
-      <p>{data.introduction}</p>
-
-      <h2>Inkluderte teknikker:</h2>
-      {data.techniques.map((technique, index) => (
-        <p key={index}>
-          <strong>{technique.name}:</strong> {technique.description}
-        </p>
-      ))}
-
-      <h2>Begrensninger og utviklingsmuligheter:</h2>
-      {data.limitations.map((limitation, index) => (
-        <p key={index}>{limitation}</p>
-      ))}
-
-      <h2>Sammendrag:</h2>
-      <p>{data.summary}</p>*/}
     </div>
   ) : (
     <></>
